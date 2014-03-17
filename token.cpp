07d59c47c0ca627e6c377fdd99bbed234fb02054
Timothy Humphries <tim@utf8.me>
2014-03-17 13:14:44 -0400

    hahaha sepples. what the heck

--- token.cpp
+++ token.cpp
@@ -1,11 +1,13 @@
 //  Simple types for Token representation.
 
 #include <cstdlib>
-#include <sys/types.h>
-#include <string.h>
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
+
+#include <sys/types.h>
+#include <string.h>
 
 enum TokenType { ID, Integer, ReservedWord, Operator, Delimiter };
 enum RWord { Class, Public, Static, Extends, Void, Int, Bool, If, Else, While,
@@ -40,14 +42,15 @@ public:
     };
 
     std::string print_repr() {
+        std::stringstream sstm;
         switch(type) {
-            // TODO: Actually write this function
-            case ID: return std::string("ID");
-            case Integer: return std::string("Integer, ");
-            case ReservedWord: return std::string("ReservedWord, ");
-            case Operator: return std::string("Operator, ");
-            case Delimiter: return std::string("Delimiter, ");
+            case ID: sstm << "ID, " << name;
+            case Integer: sstm << "Integer, " << intval;
+            case ReservedWord: sstm << "ReservedWord, " << rword_str(word);
+            case Operator: sstm << "Operator, " << oper_str(op);
+            case Delimiter: sstm << "Delimiter, ";
         }
+        return sstm.str();
     }
 
 private:
@@ -57,12 +60,55 @@ private:
     enum RWord word;
     enum OperType op;
     enum DelimType dl;
+
+    // Laborious work just for the pretty printer
+    std::string oper_str(enum OperType op) {
+        std::stringstream sstm;
+        switch(op) {
+            case Plus: sstm << "plus";
+            case Minus: sstm << "minus";
+            // TODO: Fill out cases. Not sure of format
+            // Mult, Div, LT, LEq, GEq, GT, Eq, NEq, And, Or, Not
+        }
+        return sstm.str();
+    }
+
+
+    std::string rword_str(enum RWord w) {
+        std::stringstream sstm;
+        switch(w) {
+            case Class: sstm << "class";
+            case Public: sstm << "public";
+            case Static: sstm << "static";
+            case Extends: sstm << "extends";
+            case Void: sstm << "void";
+            case Int: sstm << "int";
+            case Bool: sstm << "bool";
+            case If: sstm << "if";
+            case Else: sstm << "else";
+            case While: sstm << "while";
+            case Return: sstm << "return";
+            case Null: sstm << "null";
+            case True: sstm << "true";
+            case False: sstm << "false";
+            case This: sstm << "this";
+            case New: sstm << "new";
+            case String: sstm << "string";
+            case Main: sstm << "main";
+            case Println: sstm << "System.out.println";
+        }
+        return sstm.str();
+    }
+
+
 };
 
 void print_pgm (std::vector<Token> pgm) {
     std::vector<Token>::iterator iter;
     for (iter = pgm.begin(); iter != pgm.end(); iter++) {
         std::string str = (*iter).print_repr();
-        std::cout << str;
+        std::cout << str << '\n'; // Why doesn't this work?!!!
     }
 }
+
+
